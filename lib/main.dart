f856e9b68d2075b6e46782a7009d8b9348d8b0cc
Emcho0 <emir.kusundzija@gmail.com>
2024-12-03 09:33:26 +0100

    Dodane osnove za memory game

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,8 +10,49 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: HomePage(),
+    );
   }
 }
 
 // dodavanje elemenata igre
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color.fromARGB(255, 35, 38, 52),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: Text(
+              "Memory game",
+              style: TextStyle(
+                  fontSize: 48.6,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 205, 214, 244)),
+            ),
+          ),
+          SizedBox(
+            height: 24.0,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [],
+          )
+        ],
+      ),
+    );
+  }
+}
